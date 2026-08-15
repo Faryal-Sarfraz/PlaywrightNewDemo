d5618f78fd103a37a8280409bd1d35423bea63c3
Faryal-Sarfraz <faryalsarfraz6@gmail.com>
2026-08-15 16:45:09 +0300

    Add negative login suite

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,17 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/loginPage');
 
+test.describe('Login Tests', () => {
+
 test('login test', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.login('Admin', 'admin123');
-})
+});
+
+test('Invalid login test', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('Admin', 'wrongpassword');
+});
+});
