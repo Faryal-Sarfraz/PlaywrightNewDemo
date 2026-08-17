a018349ee77ac74a9623da48fda0ab7cb9f6f23b
Faryal-Sarfraz <faryalsarfraz6@gmail.com>
2026-08-17 10:55:07 +0300

    Add dashboard page object and assertions

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,17 +1,22 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage');
+const { DashboardPage } = require('../pages/DashboardPage');
 
 test.describe('Login Tests', () => {
 
     let loginPage;
+    let dashboardPage;
 
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page);
+        dashboardPage = new DashboardPage(page);
         await loginPage.navigate();
     });
 
 test('login test', async ({ page }) => {
        await loginPage.login('Admin', 'admin123');
+       const dashboardHeaderText = await dashboardPage.getDashboardHeaderText();
+       expect(dashboardHeaderText).toBe('Dashboard');
 });
 
 test('Invalid login test', async ({ page }) => {
