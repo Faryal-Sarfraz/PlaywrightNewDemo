7da06d0af4f777e123f7fefd4d7128bf55a7d0ac
Faryal-Sarfraz <faryalsarfraz6@gmail.com>
2026-08-27 10:59:43 +0300

    Implement Allure report setup and stabilize login tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../pages/LoginPage');
 const { DashboardPage } = require('../pages/DashboardPage');
+const { allure } = require('allure-playwright');
 
 test.describe('Login Tests', () => {
 
@@ -14,14 +15,37 @@ test.describe('Login Tests', () => {
     });
 
 test('login test', async ({ page }) => {
-       await loginPage.login();
-       const dashboardHeaderText = await dashboardPage.getDashboardHeaderText();
-       expect(dashboardHeaderText).toBe('Dashboard');
+  allure.epic('Login Tests');
+  allure.feature('Login Feature');
+  allure.story('Valid Login Test');
+  allure.description('This test verifies that a user can log in with valid credentials and access the dashboard.');
+  allure.severity('critical');
+  allure.tag('smoke');
+
+  await test.step('Perform login using env credentials', async () => {
+    await loginPage.login();
+    const loginScreenshot = await page.screenshot();
+    allure.attachment('Login page screenshot', loginScreenshot, 'image/png');
+});
+
+   await test.step('Verify dashboard heading', async () => {
+    const dashboardHeader = await dashboardPage.getDashboardHeaderText();
+    const dashboardScreenshot = await page.screenshot();
+    allure.attachment('Dashboard page screenshot', dashboardScreenshot, 'image/png');   
+    expect(dashboardHeader.trim()).toBe('Dashboard');
+  });
 });
 
 test('Invalid login test', async ({ page }) => {
-       await loginPage.login('Admin', 'wrongpassword');
-       const errorMessage = await loginPage.getErrorMessage();
-       expect(errorMessage).toBe('Invalid credentials');
+    await test.step('Attempt login with invalid credentials', async () => {
+        await loginPage.login('Admin', 'wrongpassword');
+        const invalidLoginScreenshot = await page.screenshot();
+        allure.attachment('Invalid login attempt screenshot', invalidLoginScreenshot, 'image/png');
+    });
+  
+    await test.step('Verify invalid credentials error message', async () => {
+        const errorMessage = await loginPage.getErrorMessage();
+        expect(errorMessage).toBe('Invalid credentials');
+    });
 });
 });
